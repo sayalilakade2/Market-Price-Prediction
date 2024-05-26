d576f80c463052c1d49e55fe86604c1dbf21af45
sayalilakade2 <156512833+sayalilakade2@users.noreply.github.com>
2024-05-26 17:16:03 +0530

    Update crop.py

--- crop.py
+++ crop.py
@@ -8,34 +8,53 @@ Created on Sun May 26 13:36:04 2024
 import pickle
 import numpy as np
 import streamlit as st
+import requests
+import joblib
 
 # Load the model
-loaded_model = pickle.load(open("model.pkl", 'rb'))
+#loaded_model = pickle.load(open(r"https://github.com/sayalilakade2/House_deploy-app/raw/main/finalized_model.sav", 'rb'))
+# Load the model
+model_url = "https://github.com/sayalilakade2/House_deploy-app/raw/main/finalized_model.sav"
+r = requests.get(model_url)
+
+if r.status_code == 200:
+    with open('finalized_model.sav', 'wb') as f:
+        f.write(r.content)
+else:
+    print("Failed to download the model file")
+
+model = joblib.load('finalized_model.sav')
+
 
 def DecisionTreeRegressor(input_data):
     input_data_asarray = np.asarray(input_data)
     input_data_reshaped = input_data_asarray.reshape(1, -1) 
-    prediction = loaded_model.predict(input_data_reshaped)
+    prediction = model.predict(input_data_reshaped)
     return prediction
 
 def predict_price(entries):
     try:
         # Get user input
         input_data = [int(entries[0]), float(entries[1]), int(entries[2]), int(entries[3]), 
-                      float(entries[4]), int(entries[5]), int(entries[6])]
+                      float(entries[4]), int(entries[5]), int(entries[6]), int(entries[7]), 
+                      int(entries[8]), int(entries[9]), int(entries[10]), int(entries[11])]
         
         # Perform prediction
         predicted_price = DecisionTreeRegressor(input_data)[0]
         return f"The predicted price is ${predicted_price:,.2f}"
     except ValueError:
         return "Please enter valid inputs."
-    
+
+        
+ 
+
 
 def main():
-    st.title("Market Price Prediction")
+    st.title("House Price Prediction")
     entries = []
-    for feature in ['District Name:', 'Market Name:', 'Commodity:', 'Variety:', 'Grade:', 
-                    'temp:', 'precip:']:
+    for feature in ['Bedrooms:', 'Bathrooms:', 'Sqft Living:', 'Sqft Lot:', 'Floors:', 
+                    'Waterfront:', 'View:', 'Condition:', 'Sqft Above:', 'Sqft Basement:', 
+                    'Year Built:', 'Year Renovated:']:
         entries.append(st.number_input(feature))
     
     if st.button('Predict Price'):
